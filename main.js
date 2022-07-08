00567fadde7a12e96b4fa9eaa8a0d9b8d74383a8
dingdongbyju <103965231+dingdongbyju@users.noreply.github.com>
2022-07-08 16:36:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var message = "Message complete!";
+
+var first = "Eggs ";
+var last = "Benedict";
+var full = first.concat(last);
+console.log(full);
+console.log(message);
+
+
+var upper = "uppercase ";
+var lower = "LOWERCASE";
+var upper_checked = upper.toUpperCase();
+var lower_checked = lower.toLowerCase();
+console.log(upper_checked.concat(lower_checked));
+console.log(message);
+
+
+var square_root = Math.sqrt(25);
+var maximum = Math.max(10, 20);
+var minimum = Math.min(10, 20);
+var power = Math.pow(5, 2);
+var message2 = "Calculation complete!";
+console.log(square_root);
+console.log(maximum);
+console.log(minimum);
+console.log(power);
+console.log(message2);
